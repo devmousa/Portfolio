684e1f70f857826c4f0fbe99dcb6e8ab6d9d02c3
MasterMousa <mousshamed4@gmail.com>
2023-09-09 12:50:01 +0200

    Test

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "./ubuntuFont.css";
+// import "./ubuntuFont.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
